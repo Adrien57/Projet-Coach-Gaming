3c0fb40e2603e2ec23be740ae92b10c56035b5ad
Adrien57 <adrien.musto@gmail.com>
2019-08-01 15:07:08 +0200

    requests axios home done

--- src/components/Home/CoachsListHome/index.js
+++ src/components/Home/CoachsListHome/index.js
@@ -2,99 +2,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Button, Figure, CardDeck, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 // == Import : local
 import './coachsListHome.scss';
 
 class CoachsListHome extends React.Component {
 
-  componentDidMount(){
-}
+  state= {
+    bestCoachs: [],
+  }
+
+  componentDidMount() {
+    axios.get(`http://92.243.9.86/projet-CoachsGaming-back/coach-gaming/public/coachs`)
+      .then((response) => {
+        this.setState({
+          bestCoachs: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   render() {
     return (
-    <Row>
-      <Col xs={6} md={12}>
-      <CardDeck>
-        <Card>
-          <Figure className="coach-card__figure">
-            <Figure.Image
-              className="coach-card--image"
-              width={171}
-              height={180}
-              alt="171x180"
-              src="src/assets/img/profil.jpg"
-              roundedCircle
-            />
-            <Figure.Caption>
-              Coach x
-            </Figure.Caption>
-          </Figure>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-          This is a wider card with supporting text below as a natural lead-in to
-          additional content. This content is a little bit longer.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Figure className="coach-card__figure">
-            <Figure.Image
-              className="coach-card--image"
-              width={171}
-              height={180}
-              alt="171x180"
-              src="src/assets/img/profil.jpg"
-              roundedCircle
-            />
-            <Figure.Caption>
-              Coach x
-            </Figure.Caption>
-          </Figure>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Figure className="coach-card__figure">
-            <Figure.Image
-              className="coach-card--image"
-              width={171}
-              height={180}
-              alt="171x180"
-              src="src/assets/img/profil.jpg"
-              roundedCircle
-            />
-            <Figure.Caption>
-              Coach x
-            </Figure.Caption>
-          </Figure>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-      </CardDeck>
-      </Col>
-    </Row>
+      <Row>
+        <Col xs={6} md={12}>
+          <CardDeck>
+            {this.state.bestCoachs.map(( coach ) => {
+              return (
+                <Card key={coach.id} >
+                  <Figure className="coach-card__figure">
+                    <Figure.Image
+                      className="coach-card--image"
+                      width={171}
+                      height={180}
+                      alt="171x180"
+                      src="src/assets/img/profil.jpg"
+                      roundedCircle
+                    />
+                    <Figure.Caption>
+                      {coach.rating}
+                    </Figure.Caption>
+                  </Figure>
+                  <Card.Body>
+                    <Card.Title>{coach.user.username}</Card.Title>
+                    <Card.Text>
+                      
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">{coach.team.name}</small>
+                  </Card.Footer>
+                </Card>
+              );
+            })};
+          </CardDeck>
+        </Col>
+      </Row>
     );
   }
 }
